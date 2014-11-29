d7f9ec7d14fd1ef15ebc922f2987c8c1f512f8d6
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2014-11-29 22:58:49 +0100

    fix typo

--- service.py
+++ service.py
@@ -39,7 +39,7 @@ class TravisParser(object):
 
     @cherrypy.expose
     def index(self):
-        return "Comming soon, <a href='https://github.com/buildtimetrend/service'>Buildtime Trend as a Service</a>."
+        return "Coming soon, <a href='https://github.com/buildtimetrend/service'>Buildtime Trend as a Service</a>."
 
     @cherrypy.expose
     def travis(self, repo_slug=None, build=None):
