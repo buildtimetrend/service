d281be4d12c6daefb9b7385682aff9045b5bc4a7
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2015-03-02 21:44:20 +0100

    add tests for is_repo_allowed

--- test/service_test.py
+++ test/service_test.py
@@ -26,8 +26,14 @@ import unittest
 
 
 class TestService(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         self.settings = Settings()
+
+    def setUp(self):
+        # reinit settings singleton
+        if self.settings is not None:
+            self.settings.__init__()
 
     def test_is_repo_allowed(self):
         # error is thrown when called without parameters
@@ -38,3 +44,17 @@ class TestService(unittest.TestCase):
 
         # repo is allowed by default
         self.assertTrue(is_repo_allowed("name/repo"))
+
+    def test_is_repo_allowed_set_denied(self):
+        # test denied repo
+        self.settings.add_setting("denied_repo", {"test1"})
+
+        self.assertTrue(is_repo_allowed("name/repo"))
+        self.assertFalse(is_repo_allowed("name/test1"))
+
+    def test_is_repo_allowed_set_allowed(self):
+        # test allowed repo
+        self.settings.add_setting("allowed_repo", {"test1"})
+
+        self.assertFalse(is_repo_allowed("name/repo"))
+        self.assertTrue(is_repo_allowed("name/test1"))
