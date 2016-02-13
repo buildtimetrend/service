70749d26f023a550e4f49db0365a2b3dc4605c73
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2016-02-13 16:58:44 +0100

    use ...;update required services

--- utils/service_daemon.sh
+++ utils/service_daemon.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 ### BEGIN INIT INFO
 # Provides:          btt_service
-# Required-Start:    $local_fs $network $named $time $syslog
-# Required-Stop:     $local_fs $network $named $time $syslog
+# Required-Start:    $local_fs $network $time $syslog
+# Required-Stop:     $local_fs $network $time $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Description:       Buildtime Trend as a Service daemon
@@ -18,7 +18,7 @@ start() {
     echo 'Service already running' >&2
     return 1
   fi
-  echo 'Starting service…' >&2
+  echo 'Starting service...' >&2
   `$SCRIPT`
   echo 'Service started' >&2
 }
@@ -28,7 +28,7 @@ stop() {
     echo 'Service not running' >&2
     return 1
   fi
-  echo 'Stopping service…' >&2
+  echo 'Stopping service...' >&2
   kill -15 $(cat "$PIDFILE")
   echo 'Service stopped' >&2
 }
